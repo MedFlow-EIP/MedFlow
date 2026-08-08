ac98189681f1e05aa6742fabb89a615cc6b294a1
Wavitoo <arslan.tetu@epitech.eu>
2026-08-08 21:49:34 +0200

    feat(mobile): branche TutorialProvider et navigationRef

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,14 +3,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { AppNavigator } from './components/navigation/AppNavigator';
 import { ThemeProvider } from './theme/ThemeContext';
+import { TutorialProvider } from './context/TutorialContext';
+import { navigationRef } from './navigationRef';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <ThemeProvider>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
+        <TutorialProvider>
+          <NavigationContainer ref={navigationRef}>
+            <AppNavigator />
+          </NavigationContainer>
+        </TutorialProvider>
       </ThemeProvider>
     </GestureHandlerRootView>
   );
