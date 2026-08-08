43c0fce0fd121da73c9605dfda376d5df8a72a03
Wavitoo <arslan.tetu@epitech.eu>
2026-08-08 21:00:19 +0200

    feat(mobile): infra dark mode (ThemeContext + palette)

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { AppNavigator } from './components/navigation/AppNavigator';
+import { ThemeProvider } from './theme/ThemeContext';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
+      <ThemeProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </ThemeProvider>
     </GestureHandlerRootView>
   );
 }
